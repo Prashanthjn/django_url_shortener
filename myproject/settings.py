4bb2e6c12c6392ef991549e334483ba62cf05b7c
Prashanth J N <prashi8711@example.com>
2026-02-24 06:14:47 +0530

    Added production settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jh1*8%l!*#4r%9d2(^@@dr76hv9114b41uynke#-sl%sp25my#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1']
+
 
 
 # Application definition
